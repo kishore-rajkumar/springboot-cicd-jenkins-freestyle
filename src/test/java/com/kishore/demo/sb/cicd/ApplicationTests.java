97fcb8fdc128762c3078bcc54db774fe25bd51dc
Dev Profile <kishore.rajkumar@hotmail.com>
2022-09-17 21:11:11 +0530

    project added

---
+++ src/test/java/com/kishore/demo/sb/cicd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.demo.sb.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
